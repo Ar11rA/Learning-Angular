0542c153d7b11d10432d33103aca2ecb495db147
Ar11rA <aritra119.26@gmail.com>
2018-06-16 23:28:52 +0530

    Added basic design template for login and register

--- angular-firebase/src/app/material.module.ts
+++ angular-firebase/src/app/material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {
   MatButtonModule,
   MatMenuModule,
@@ -16,7 +16,8 @@ import {
     MatToolbarModule,
     MatIconModule,
     MatCardModule,
-    MatInputModule
+    MatInputModule,
+    BrowserAnimationsModule,
   ],
   exports: [
     MatButtonModule,
